e23aa6b1062c4bf2422dfebc976d403a2f16b9b1
chenyinyu <chenyinyu@oppo.com>
2020-04-13 18:59:54 +0800

    no message

---
+++ mylib/src/main/java/com/example/mylib/TestPerson.java
@@ -0,0 +1,32 @@
+package com.example.mylib;
+
+/*********************************************************** 
+ ** Copyright (C), 2008-2017, OPPO Mobile Comm Corp., Ltd. 
+ ** VENDOR_EDIT ** File: - TestPerson.java 
+ ** Description: XXXXXXXXXXXXXXXXXXXXX. 
+ ** Version: 1.0 
+ ** Date : 2020/4/13  
+ ** Author: 80240190@TAG1.TAG2.TAG3.TAG4[.TAG5.TAG6.TAG7] 
+ ** ** ---------------------Revision History: --------------------- 
+ ** ** 80240190       2020/4/13     1.0 build this module  
+ ****************************************************************/
+public class TestPerson {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
